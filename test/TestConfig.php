bdb83b0114aee18a39db0eb171dc1ddf7a4a860c
Dennes B Abing <dennes.b.abing@gmail.com>
2012-11-23 20:17:05 +0800

    Fix Test folder

---
+++ test/TestConfig.php
@@ -0,0 +1,9 @@
+<?php
+define('ZF2_PATH', __DIR__ . '/../../../../vendor/zendframework/zendframework/library');
+define('ZF2_MODULES_TEST_PATHS', __DIR__ .'/../../../../vendor');
+
+return array(
+    'modules' => array(
+        'DxUser'
+    ),
+);
